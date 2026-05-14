a73d74aa5273c830c6c9d600340d91bed6528891
nort-17 <anbzimports@gmail.com>
2026-05-14 20:30:22 +0700

    Harden Smart Swap Finder fallback

---
+++ _next/static/vx6XfwBebH75fqWpJO7oO/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nort-smart-swap-finder//_next/:path+",
+        "destination": "/nort-smart-swap-finder/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
